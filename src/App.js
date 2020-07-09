2ce8737a0c60c5a5f1558c7c24180ce7cbd8031b
netolopes <netolopes27@gmail.com>
2020-07-09 09:36:49 -0300

    meu primeiro commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React,{useState} from 'react';
+import  UserTable from './components/UserTable';
+import  AddUserForm from './components/AddUserForm';
+import  EditUserForm from './components/EditUserForm';
+import {v4 as uuidv4 } from 'uuid';
+
+function App() {
+
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+  ]
+
+  //state
+  const [users,setUsers] = useState(usersData);
+
+  //adicionar usuarios
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([...users, user])
+  }
+//deletar
+const deleteUser = (id) => {
+  setUsers(users.filter((user) => user.id !== id))
+}
+
+//editar
+const [editing,setEditing] = useState(false);
+const [currentUser,setCurrentUser] = useState({
+  id:null,name:'',username:''
+});
+const editRow = (user) => {
+  setEditing(true);
+  setCurrentUser({
+    id: user.id,name: user.name, username: user.username
+  })
+  
+}
+
+const updateUser = (id, updateUser) =>{
+  setEditing(false);
+  setUsers(users.map(user =>(user.id === id ? updateUser : user)))
+}
+
+
+  return (
+    <div className="container">
+    <h1>CRUD App with Hooks</h1>
+    <div className="flex-row">
+      <div className="flex-large">
+        {
+          editing ? (
+            <div>
+            <h2>Edit user</h2>
+            <EditUserForm currentUser={currentUser} updateUser={updateUser} />
+            </div>
+          ) : (
+            <div>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
+          </div>
+
+          )
+        }
+       
+      </div>
+      <div className="flex-large">
+        <h2>View users</h2>
+        <UserTable users={users}
+         deleteUser={deleteUser}
+         editRow={editRow} />
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
